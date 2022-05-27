d0ca0c4c0c06cee2095cabe05f5fbecc5b1a8013
sanzharsp <99716421+sanzharsp@users.noreply.github.com>
2022-05-27 14:28:25 +0600

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'azyqorgs_backend',
+        'USER': 'azyqorgs_backend',
+        'PASSWORD': 'Azyq@#MainSIS000%$',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
